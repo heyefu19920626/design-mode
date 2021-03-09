8fd23bb9c857adeea8dd75d90d04d20860315152
heyefu 30002440 <heyefu@huawei.com>
2021-03-09 15:33:59 +0800

    leetcode:1047, 栈

---
+++ src/test/java/test/leetcode/editor/cn/RemoveAllAdjacentDuplicatesInString.java
@@ -0,0 +1,56 @@
+package test.leetcode.editor.cn;
+
+//给出由小写字母组成的字符串 S，重复项删除操作会选择两个相邻且相同的字母，并删除它们。 
+//
+// 在 S 上反复执行重复项删除操作，直到无法继续删除。 
+//
+// 在完成所有重复项删除操作后返回最终的字符串。答案保证唯一。 
+//
+// 
+//
+// 示例： 
+//
+// 输入："abbaca"
+//输出："ca"
+//解释：
+//例如，在 "abbaca" 中，我们可以删除 "bb" 由于两字母相邻且相同，这是此时唯一可以执行删除操作的重复项。之后我们得到字符串 "aaca"，其中又
+//只有 "aa" 可以执行重复项删除操作，所以最后的字符串为 "ca"。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= S.length <= 20000 
+// S 仅由小写英文字母组成。 
+// 
+// Related Topics 栈
+
+
+import java.util.Stack;
+
+public class RemoveAllAdjacentDuplicatesInString {
+    public static void main(String[] args) {
+        Solution solution = new RemoveAllAdjacentDuplicatesInString().new Solution();
+        String target = "abbaca";
+        String result = solution.removeDuplicates(target);
+        System.out.println(result);
+    }
+
+    class Solution {
+        public String removeDuplicates(String S) {
+            Stack<Character> stack = new Stack<>();
+            for (int i = 0; i < S.length(); i++) {
+                if (stack.isEmpty() || stack.peek() != S.charAt(i)) {
+                    stack.push(S.charAt(i));
+                } else {
+                    stack.pop();
+                }
+            }
+            StringBuilder builder = new StringBuilder();
+            stack.forEach(builder::append);
+            return builder.toString();
+        }
+    }
+}
